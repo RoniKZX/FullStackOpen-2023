054bb8d5ea2c37cf4738ce0beccfaee9f6addfc7
kal <101020782+RoniKZX@users.noreply.github.com>
2023-05-11 11:34:23 -0400

    Counters and re-rendering
    Do not use the "refresh" function strategy as it
    is not an efficient way of re-render a webpage.
    You may see a better way later.

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -2,12 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let counter = 1;
+
+function refresh() {
+  const root = ReactDOM.createRoot(document.getElementById('root'))
+  root.render(
+    <React.StrictMode>
+      <App counter={counter}/>
+    </React.StrictMode>
+  )
+};
+
+setInterval(() => {
+  refresh()
+  counter += 1;
+}, 2000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
