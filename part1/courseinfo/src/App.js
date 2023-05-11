778501637f3ac0a8e138dd38119547b2445a76f6
kal <101020782+RoniKZX@users.noreply.github.com>
2023-05-11 13:36:55 -0400

    Add buttons to control the counter

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,16 +1,21 @@
 import { useState } from "react";
 
+const Display = ({counter}) => <p>{counter}</p>
+const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
+
 function App() {
   const [counter, setCounter] = useState(0)
 
   const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
   const resetCounter = () => setCounter(0)
 
   return (
     <>
-      {counter}
-      <button onClick={increaseByOne}>Increment</button>
-      <button onClick={resetCounter}>Reset to zero</button>
+      <Display counter={counter} />
+      <Button handleClick={increaseByOne} text="Increase" />
+      <Button handleClick={decreaseByOne} text="Decrease" />
+      <Button handleClick={resetCounter} text="Reset" />
     </>
   );
 }
