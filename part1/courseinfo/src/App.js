6f47bceb1579952ecdb384a187a996f3cc0904ab
kal <101020782+RoniKZX@users.noreply.github.com>
2023-05-11 14:17:44 -0400

    Conditional rendering
    Important: The update of an state is asynchronous
    
    It means, that a state update in React does not happen immediately
    but "at some point" before the component is rendered again.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,21 +1,45 @@
 import { useState } from "react";
 
-const Display = ({counter}) => <p>{counter}</p>
-const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
+// const Display = ({ counter }) => <p>{counter}</p>
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
+
+function History({ allClicks }) {
+  if (allClicks.length === 0) {
+    return (<p>The app is used by pressing the buttons.</p>)
+  }
+  return (<p>Button press history: [{allClicks.join(' ')}]</p>)
+}
 
 function App() {
-  const [counter, setCounter] = useState(0)
+  const [clicks, setClicks] = useState({
+    left: 0,
+    right: 0
+  })
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const resetCounter = () => setCounter(0)
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = clicks.left + 1
+    setClicks({ ...clicks, left: updatedLeft })
+    setTotal(updatedLeft + clicks.right)
+  }
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = clicks.right + 1
+    setClicks({ ...clicks, right: updatedRight })
+    setTotal(clicks.left + updatedRight)
+  }
 
   return (
     <>
-      <Display counter={counter} />
-      <Button handleClick={increaseByOne} text="Increase" />
-      <Button handleClick={decreaseByOne} text="Decrease" />
-      <Button handleClick={resetCounter} text="Reset" />
+      {clicks.left}
+      <Button handleClick={handleLeftClick} text="Left" />
+      <Button handleClick={handleRightClick} text="Right" />
+      {clicks.right}
+
+      <History allClicks={allClicks} />
+      <p>Total clicks: {total}</p>
     </>
   );
 }
