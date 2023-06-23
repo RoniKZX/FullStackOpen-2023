63e16f2a04930bcb40356cdecbe10bf13500a722
kal <101020782+RoniKZX@users.noreply.github.com>
2023-06-23 12:29:38 -0400

    Validation and ESLint

--- part3/notes/backend/index.js
+++ part3/notes/backend/index.js
@@ -24,24 +24,6 @@ app.get('/api/notes', (req, res) => {
   Note.find({}).then(notes => res.json(notes))
 })
 
-
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
-  }
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-  })
-
-  note.save().then(savedNote => response.json(note))
-})
-
 app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
     .then(note => {
@@ -54,15 +36,27 @@ app.get('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.put('/api/notes/:id', (request, response, next) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
-  const note = {
+  const note = new Note({
     content: body.content,
-    important: body.important
-  }
+    important: body.important || false,
+  })
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  note.save()
+    .then(savedNote => response.json(note))
+    .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
+
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  )
     .then(updatedNote => {
       response.json(updatedNote)
     })
@@ -88,8 +82,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
-
   next(error)
 }
 
